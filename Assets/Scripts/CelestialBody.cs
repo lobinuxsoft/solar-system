bf940c9c5186150a541781341f636c63b7b8ac60
Matias <lobinuxsoft@gmail.com>
2022-03-15 13:00:45 -0300

    Some changes in scripts

---
+++ Assets/Scripts/CelestialBody.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class CelestialBody : MonoBehaviour
+{
+    [SerializeField] float mass;
+    [SerializeField] float radius;
+    [SerializeField] Vector3 initialVelocity;
+
+    Vector3 currentVelocity;
+
+    private void Awake()
+    {
+        currentVelocity = initialVelocity;
+    }
+
+    public void UpdateVelocity(CelestialBody[] allBodies, float timeStep)
+    {
+        foreach (var otherBody in allBodies)
+        {
+            if (otherBody != this)
+            {
+
+            }
+        }
+    }
+}
